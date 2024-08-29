381a4a6afd9d70a4ea366a4277ad4948ffffc217
sachin singh <sachinthegreat1001@gmail.com>
2024-08-29 18:27:26 +0530

    ashish correction

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Ashish></Ashish> singh
+          sachin singh great singh
         </a>
       </header>
     </div>
