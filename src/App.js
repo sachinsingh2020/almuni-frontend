6cde7c5a14fcc59424649bca5c7a9ed79dcb6565
sachin singh <sachinthegreat1001@gmail.com>
2024-08-29 23:17:54 +0530

    my react router changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useEffect } from 'react';
+import toast, { Toaster } from 'react-hot-toast';
+import { ProtectedRoute } from 'protected-route-react';
+import Home from './Components/Home';
 import Login from './Components/Login';
 
-
 function App() {
+  const { isAuthenticated, error } = useSelector(state => state.user);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (error) {
+      toast.error(error);
+      dispatch({ type: 'clearError' });
+    }
+  }, [dispatch, error]);
+
   return (
-    <div className="App">
-     <Login/>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/login" element={
+          <ProtectedRoute
+            isAuthenticated={!isAuthenticated}
+            redirect="/"
+          >
+            <Login />
+          </ProtectedRoute>} />
+
+        <Route exact path="/" element={
+          <ProtectedRoute
+            isAuthenticated={isAuthenticated}
+            redirect="/login"
+          >
+            <Home />
+          </ProtectedRoute>} />
+
+      </Routes>
+      <Toaster />
+    </Router>
   );
 }
 
