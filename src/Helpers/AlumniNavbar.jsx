c02f48806f098b4ef18a4ff51fc5e1c910fcf0df
sachin singh <sachinthegreat1001@gmail.com>
2024-09-04 10:08:40 +0530

    added some link tags

--- src/Helpers/AlumniNavbar.jsx
+++ src/Helpers/AlumniNavbar.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { alumniLogout } from '../redux/actions/alumni';
 import toast from 'react-hot-toast';
 
 const AlumniNavbar = () => {
 
     const dispatch = useDispatch();
+    const navigate = useNavigate();
 
     const handleLogout = () => {
         dispatch(alumniLogout());
@@ -26,15 +27,28 @@ const AlumniNavbar = () => {
         }
     }, [dispatch, alumniError, message])
 
+    const { isAuthenticated } = useSelector(state => state.user);
+
+    const handleHome = () => {
+        if (isAuthenticated) {
+            navigate("/student-home")
+        } else {
+            navigate("/alumni-home")
+        }
+    }
 
     return (
         <header className="mb-4 py-6 mx-12">
             <div className="max-w-8xl mx-auto py-4 flex justify-between items-center">
                 <h1 className="text-3xl font-bold text-gray-800">Alumni Connect</h1>
                 <nav className="space-x-12">
-                    <Link to="/alumni-home" className="text-gray-900 font-semibold">
+
+                    <button
+                        onClick={handleHome}
+                        className="text-gray-900 font-semibold"
+                    >
                         Home
-                    </Link>
+                    </button>
                     <Link to="/job-portal" className="text-gray-900 font-semibold">
                         Job Portal
                     </Link>
--- src/redux/store.js
+++ src/redux/store.js
@@ -13,5 +13,5 @@ const store = configureStore({
 
 export default store
 
-export const server = "https://alumni-backend-gray.vercel.app/api/v1"
-// export const server = "http://localhost:4000/api/v1"
+// export const server = "https://alumni-backend-gray.vercel.app/api/v1"
+export const server = "http://localhost:4000/api/v1"
